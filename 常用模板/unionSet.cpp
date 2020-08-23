04b17d4fd3ca49e2ce4a9e53c53603983f909825
wilson_t <Wilson.T@sjtu.edu.cn>
2020-08-23 16:28:52 +0800

    TW 2020.8.23

--- 常用模板/unionSet.cpp
+++ 常用模板/unionSet.cpp
@@ -35,3 +35,39 @@ public:
         }
     }
 };
+
+class UF {
+public:
+    vector<int> fa;
+    vector<int> sz;
+    int n;
+    int comp_cnt;
+    
+public:
+    UF(int _n): n(_n), comp_cnt(_n), fa(_n), sz(_n, 1) {
+        iota(fa.begin(), fa.end(), 0);
+    }
+    
+    int findset(int x) {
+        return fa[x] == x ? x : fa[x] = findset(fa[x]);
+    }
+    
+    void unite(int x, int y) {
+        if (sz[x] < sz[y]) {
+            swap(x, y);
+        }
+        fa[y] = x;
+        sz[x] += sz[y];
+        --comp_cnt;
+    }
+    
+    bool findAndUnite(int x, int y) {
+        int x0 = findset(x);
+        int y0 = findset(y);
+        if (x0 != y0) {
+            unite(x0, y0);
+            return true;
+        }
+        return false;
+    }
+};
