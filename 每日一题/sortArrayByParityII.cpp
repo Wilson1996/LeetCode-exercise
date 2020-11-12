35d30b5d94c7203e176b0bccc2b43ebb1075512d
wilson_t <Wilson.T@sjtu.edu.cn>
2020-11-12 14:31:10 +0800

    TW 2020.11.12

---
+++ 每日一题/sortArrayByParityII.cpp
@@ -0,0 +1,41 @@
+/*
+* @Author: wilson_t
+* @Date:   2020-11-12 14:29:30
+* @Last Modified by:   wilson_t
+* @Last Modified time: 2020-11-12 14:30:17
+*/
+/*********************************************************
+ * 题目[简单]：
+给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
+对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+你可以返回任何满足上述条件的数组作为答案。
+ 
+示例：
+输入：[4,2,5,7]
+输出：[4,5,2,7]
+解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+
+提示：
+2 <= A.length <= 20000
+A.length % 2 == 0
+0 <= A[i] <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-array-by-parity-ii
+*********************************************************/
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& A) {
+        int n = A.size();
+        int j = 1;
+        for (int i = 0; i < n; i += 2) {
+            if (A[i] % 2 == 1) {
+                while (A[j] % 2 == 1) {
+                    j += 2;
+                }
+                swap(A[i], A[j]);
+            }
+        }
+        return A;
+    }
+};
